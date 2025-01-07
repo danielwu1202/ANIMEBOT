79242538748616f72ab582a0750c0de93ec0cf20
Daniel Wu <danielwu1202@gmail.com>
2025-01-07 22:13:56 +0800

    add log

--- bot_1.0.py
+++ bot_1.0.py
@@ -14,8 +14,9 @@ LINE_CHANNEL_SECRET = '你的 Channel Secret'
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-print(line_bot_api)
-print(handler)
+print('config')
+print(LINE_CHANNEL_ACCESS_TOKEN)
+print(LINE_CHANNEL_SECRET)
 
 
 # Webhook 路徑，用來接收 Line 平台的事件
